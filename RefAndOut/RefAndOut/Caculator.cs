b8e6b52da9eb904b0da000c8271d1392ac1a791a
unknown <faezehjafarpour75@gmail.com>
2022-06-02 16:10:52 +0430

    ref & out

---
+++ RefAndOut/RefAndOut/Caculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefAndOut
+{
+    public class Caculator
+    {
+
+        public void GetRefValue(ref int Number)
+        {
+            Console.WriteLine("NumbeRef is: " + ++Number);
+        }
+
+        internal void GetOutValue(out int numberOut)
+        {
+            numberOut = 1;
+            Console.WriteLine("numberOut is: " + ++numberOut);
+        }
+    }
+}
---
+++ RefAndOut/RefAndOut/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RefAndOut;
+
+Console.WriteLine("Hello, World!");
+
+
+//ref
+int NumbeRef = 0;
+Console.WriteLine("NumbeRef is: " + NumbeRef);
+Caculator caculator = new Caculator();
+caculator.GetRefValue(ref NumbeRef);
+
+Console.WriteLine("NumbeRef is: " + NumbeRef);
+
+
+//out
+int NumberOut;
+caculator.GetOutValue(out NumberOut);
+
+Console.WriteLine("NumberOut is: " + NumberOut);
+Console.WriteLine("NumberOut is: " + ++NumberOut);
+
+Console.ReadKey();
